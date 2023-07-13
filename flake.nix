039df91e98884f33fd04ed69409b4303786a853d
Anselm Schüler <mail@anselmschueler.com>
2023-07-13 15:50:32 +0200

    nixify

--- flake.nix
+++ flake.nix
@@ -7,6 +7,15 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "3darr";
+          version = "unstable";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp 3darr $out/bin/3darr
+          '';
+        };
         devShells.default = pkgs.stdenv.mkDerivation {
           name = "env";
         };
