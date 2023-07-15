b7a840b60ed573b6e05418fc49e2fda860d651e4
Anselm Schüler <mail@anselmschueler.com>
2023-07-15 21:04:38 +0200

    Improve?

--- 3darr.c
+++ 3darr.c
@@ -1,11 +1,14 @@
 #include <errno.h>
+#include <inttypes.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-bool report(size_t allocs) {
+typedef unsigned long elem;
+
+bool print_allocs(size_t allocs) {
     if (printf("successfully allocated %zu times\n", allocs) < 0) {
         perror("value output");
         return false;
@@ -13,22 +16,23 @@ bool report(size_t allocs) {
     return true;
 }
 
-unsigned long getularg(int argno, char *argid, char **argv) {
+size_t get_arg_size_t(int argno, char *argid, char **argv) {
     char *endptr;
     errno = 0;
-    unsigned long val = strtoul(argv[argno], &endptr, 10);
+    uintmax_t val = strtoumax(argv[argno], &endptr, 10);
     if (argv[1][0] == '\0' || *endptr != '\0') {
         fprintf(stderr, "failed to parse argument %s\n", argid);
         exit(EXIT_FAILURE);
     }
-    if (errno == ERANGE) {
+    if (errno == ERANGE || val > SIZE_MAX) {
         fprintf(stderr, "argument %s is too large\n", argid);
         exit(EXIT_FAILURE);
     }
-    return val;
+    return (size_t)val;
 }
 
-void cleanup(unsigned long ***arr, size_t x, size_t y, size_t i, size_t j) {
+void free_and_exit(elem ***arr, size_t x, size_t y, size_t i, size_t j,
+                   int ecode) {
     for (size_t i_ = 0; i_ < i; i_++) {
         for (size_t j_ = 0; j_ < y; j_++)
             free(arr[i_][j_]);
@@ -40,64 +44,74 @@ void cleanup(unsigned long ***arr, size_t x, size_t y, size_t i, size_t j) {
         free(arr[i]);
     }
     free(arr);
+    exit(ecode);
 }
 
-int main(int argc, char **argv) {
+elem ***mk_arr(size_t x, size_t y, size_t z, size_t *allocs) {
+    *allocs = 0;
+    elem ***arr = malloc(x * sizeof(elem **));
+    if (arr == NULL) {
+        perror("array allocation");
+        print_allocs(*allocs);
+        exit(EXIT_FAILURE);
+    }
+    ++*allocs;
+    for (size_t i = 0; i < x; i++) {
+        arr[i] = malloc(y * sizeof(elem *));
+        if (arr[i] == NULL) {
+            perror("array allocation");
+            print_allocs(*allocs);
+            free_and_exit(arr, x, y, i, 0, EXIT_FAILURE);
+        }
+        ++*allocs;
+        for (size_t j = 0; j < y; j++) {
+            arr[i][j] = malloc(z * sizeof(elem));
+            if (arr[i][j] == NULL) {
+                perror("array allocation");
+                print_allocs(*allocs);
+                free_and_exit(arr, x, y, i, j, EXIT_FAILURE);
+            }
+            ++*allocs;
+            for (size_t k = 0; k < z; k++)
+                arr[i][j][k] = pow(2, i) + pow(3, j) + pow(5, k);
+        }
+    }
+    return arr;
+}
+
+void ensure_usage(int argc, char **argv) {
     if (argc != 4) {
         char *pname = argc == 0 ? "<program>" : argv[0];
         fprintf(stderr,
                 "wrong usage!\n"
                 "usage: %s <x> <y> <z>\n",
                 pname);
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
-    unsigned long x = getularg(1, "x", argv);
-    unsigned long y = getularg(2, "y", argv);
-    unsigned long z = getularg(3, "z", argv);
-    size_t allocs = 0;
-    unsigned long ***arr = malloc(x * sizeof(unsigned long **));
-    if (arr == NULL) {
-        perror("array allocation");
-        report(allocs);
-        return EXIT_FAILURE;
-    }
-    allocs++;
-    for (size_t i = 0; i < x; i++) {
-        arr[i] = malloc(y * sizeof(unsigned long *));
-        if (arr[i] == NULL) {
-            perror("array allocation");
-            report(allocs);
-            cleanup(arr, x, y, i, 0);
-            return EXIT_FAILURE;
-        }
-        allocs++;
-        for (size_t j = 0; j < y; j++) {
-            arr[i][j] = malloc(z * sizeof(unsigned long));
-            if (arr[i][j] == NULL) {
-                perror("array allocation");
-                report(allocs);
-                cleanup(arr, x, y, i, j);
-                return EXIT_FAILURE;
-            }
-            allocs++;
-            for (size_t k = 0; k < z; k++)
-                arr[i][j][k] = pow(2, i) + pow(3, j) + pow(5, k);
-        }
-    }
-    if (!report(allocs)) {
-        perror("value output");
-        cleanup(arr, x, y, x, y);
-        return EXIT_FAILURE;
-    }
+}
+
+void printarr(elem ***arr, size_t x, size_t y, size_t z) {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
             for (size_t k = 0; k < z; k++)
                 if (printf("arr[%zu][%zu][%zu] = %lu\n", i, j, k,
                            arr[i][j][k]) < 0) {
                     perror("value output");
-                    cleanup(arr, x, y, x, y);
-                    return EXIT_FAILURE;
+                    free_and_exit(arr, x, y, x, y, EXIT_FAILURE);
                 }
-    cleanup(arr, x, y, x, y);
-    return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+    ensure_usage(argc, argv);
+    size_t x = get_arg_size_t(1, "x", argv);
+    size_t y = get_arg_size_t(2, "y", argv);
+    size_t z = get_arg_size_t(3, "z", argv);
+    size_t allocs;
+    elem ***arr = mk_arr(x, y, z, &allocs);
+    if (!print_allocs(allocs)) {
+        perror("value output");
+        free_and_exit(arr, x, y, x, y, EXIT_FAILURE);
+    }
+    printarr(arr, x, y, z);
+    free_and_exit(arr, x, y, x, y, EXIT_SUCCESS);
 }
